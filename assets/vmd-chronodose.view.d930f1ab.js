652c602d9331689baf06254d8c1cd323a4424b7c
Pierre Mesure <pierre@mesu.re>
2021-05-25 15:11:38 +0200

    Remove 1177's logo

--- assets/vmd-chronodose.view.d930f1ab.js
+++ assets/vmd-chronodose.view.d930f1ab.js
@@ -1,4 +1,4 @@
-import{L as e,h as r,C as t,d as s,e as o}from"./index.31c4ccad.js";import"./vendor.1d43dcfd.js";var a=Object.defineProperty,c=Object.getOwnPropertyDescriptor;let d=class extends e{render(){return r`
+import{L as e,h as r,C as t,d as s,e as o}from"./index.ea76e576.js";import"./vendor.1d43dcfd.js";var a=Object.defineProperty,i=Object.getOwnPropertyDescriptor;let n=class extends e{render(){return r`
             <slot name="chronodose"></slot>
 
             <div class="homeCard-actions">
@@ -8,9 +8,9 @@ import{L as e,h as r,C as t,d as s,e as o}from"./index.31c4ccad.js";import"./ven
                     </a>
                 </div>
             </div>
-        `}};d.styles=[t,s`
+        `}};n.styles=[t,s`
             :host {
                 display: block;
             }
             #mapid { height: 180px; }
-        `],d=((e,r,t,s)=>{for(var o,d=s>1?void 0:s?c(r,t):r,i=e.length-1;i>=0;i--)(o=e[i])&&(d=(s?o(r,t,d):o(d))||d);return s&&d&&a(r,t,d),d})([o("vmd-chronodose")],d);export{d as VmdChronodoseView};
+        `],n=((e,r,t,s)=>{for(var o,n=s>1?void 0:s?i(r,t):r,d=e.length-1;d>=0;d--)(o=e[d])&&(n=(s?o(r,t,n):o(n))||n);return s&&n&&a(r,t,n),n})([o("vmd-chronodose")],n);export{n as VmdChronodoseView};
