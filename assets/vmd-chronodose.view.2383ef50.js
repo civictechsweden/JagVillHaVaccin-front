955d79628111509e67567adf6739c34b240d1fcd
Pierre Mesure <pierre@mesu.re>
2021-06-07 08:51:17 +0200

    Reenable all the regions and change the message

--- assets/vmd-chronodose.view.2383ef50.js
+++ assets/vmd-chronodose.view.2383ef50.js
@@ -1,4 +1,4 @@
-import{L as e,h as r,C as t,d as s,e as o}from"./index.70806426.js";import"./vendor.1d43dcfd.js";var a=Object.defineProperty,i=Object.getOwnPropertyDescriptor;let n=class extends e{render(){return r`
+import{L as e,h as r,C as t,d as s,e as o}from"./index.928c674c.js";import"./vendor.1d43dcfd.js";var a=Object.defineProperty,i=Object.getOwnPropertyDescriptor;let n=class extends e{render(){return r`
             <slot name="chronodose"></slot>
 
             <div class="homeCard-actions">
@@ -13,4 +13,4 @@ import{L as e,h as r,C as t,d as s,e as o}from"./index.70806426.js";import"./ven
                 display: block;
             }
             #mapid { height: 180px; }
-        `],n=((e,r,t,s)=>{for(var o,n=s>1?void 0:s?i(r,t):r,d=e.length-1;d>=0;d--)(o=e[d])&&(n=(s?o(r,t,n):o(n))||n);return s&&n&&a(r,t,n),n})([o("vmd-chronodose")],n);export{n as VmdChronodoseView};
+        `],n=((e,r,t,s)=>{for(var o,n=s>1?void 0:s?i(r,t):r,c=e.length-1;c>=0;c--)(o=e[c])&&(n=(s?o(r,t,n):o(n))||n);return s&&n&&a(r,t,n),n})([o("vmd-chronodose")],n);export{n as VmdChronodoseView};
