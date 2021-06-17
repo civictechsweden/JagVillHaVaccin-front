a0b856b019f2c51ad7baf112f0148e8fd7df6f89
Pierre Mesure <pierre@mesu.re>
2021-06-17 11:54:53 +0200

    Fix the texts and change the button text

--- assets/vmd-apropos.view.68e2fd49.js
+++
@@ -1,4 +0,0 @@
-import{L as e,h as r,d as t,e as c}from"./index.c52cafd4.js";import"./vendor.1d43dcfd.js";var o=Object.defineProperty,s=Object.getOwnPropertyDescriptor;let a=class extends e{constructor(){super()}render(){return r`
-            <slot name="about"></slot>
-        `}connectedCallback(){super.connectedCallback()}disconnectedCallback(){super.disconnectedCallback()}};a.styles=[t`
-        `],a=((e,r,t,c)=>{for(var a,n=c>1?void 0:c?s(r,t):r,d=e.length-1;d>=0;d--)(a=e[d])&&(n=(c?a(r,t,n):a(n))||n);return c&&n&&o(r,t,n),n})([c("vmd-apropos")],a);export{a as VmdAproposComponent};
