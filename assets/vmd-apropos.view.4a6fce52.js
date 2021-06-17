6424075d30f2e53ce86580b368455b21a362729a
Pierre Mesure <pierre@mesu.re>
2021-06-17 12:29:54 +0200

    Fix how the times are shown for Västerbotten and Västmanland. Added missing 'sedan'

--- assets/vmd-apropos.view.4a6fce52.js
+++
@@ -1,4 +0,0 @@
-import{L as e,h as r,d as t,e as o}from"./index.2cb14bbb.js";import"./vendor.1d43dcfd.js";var s=Object.defineProperty,c=Object.getOwnPropertyDescriptor;let n=class extends e{constructor(){super()}render(){return r`
-            <slot name="about"></slot>
-        `}connectedCallback(){super.connectedCallback()}disconnectedCallback(){super.disconnectedCallback()}};n.styles=[t`
-        `],n=((e,r,t,o)=>{for(var n,a=o>1?void 0:o?c(r,t):r,d=e.length-1;d>=0;d--)(n=e[d])&&(a=(o?n(r,t,a):n(a))||a);return o&&a&&s(r,t,a),a})([o("vmd-apropos")],n);export{n as VmdAproposComponent};
